b8239ed61717e52788a60efbd2a11d49114d21a0
Megood1210 <xuanvinhle06@gmail.com>
2025-03-05 20:34:27 +0700

    Upload bài tập 3

---
+++ ss21ex3.js
@@ -0,0 +1,7 @@
+let password = "Hello123";
+let enterPass = prompt("Nhap mat khau");
+if(enterPass == password){
+    document.write("Mat khau dung");
+}else{
+    document.write("Mat khau sai");
+}
