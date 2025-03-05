30425eb2da0d09fe1346b1347d04921683889018
Megood1210 <xuanvinhle06@gmail.com>
2025-03-05 20:36:14 +0700

    Upload bài tập 5

---
+++ ss21ex5.js
@@ -0,0 +1,12 @@
+let number = 0;
+for(let i =1; i<=100;i++){
+    if(i %3 == 0){
+        document.write("Fizz");
+    }else if(i % 5 === 0 ){
+        document.write("Buzz");
+    }else if(i % 3 === 0 && i % 5 ==0){
+        document.write("FizzBuzz");
+    }else{
+        document.write(i);
+    }
+}
